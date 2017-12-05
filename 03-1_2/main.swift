7475d58a00ccf5fdb284c8875d0e715bec94123b
Kevin Heinicke <kevin@kehei.de>
2017-12-05 08:49:33 +0100

    try to calculate some distances

--- 03-1_2/main.swift
+++ 03-1_2/main.swift
@@ -6,6 +6,7 @@ class GridEdge {
   let value: Int
   let corner: Int  // position of edge
   let distance: Int  // distance to previous edge, aka. width of square
+  let radius: Int
   let previousEdge: GridEdge?
   var nextEdge: GridEdge?
 
@@ -17,10 +18,12 @@ class GridEdge {
         // outside
         self.corner = 0
         self.distance = self.previousEdge!.distance + 2
+        self.radius = self.previousEdge!.radius + 1
       } else {
         // just move through the edges, the distances stays the same
         self.corner = self.previousEdge!.corner + 1
         self.distance = self.previousEdge!.distance
+        self.radius = self.previousEdge!.radius
       }
       self.value = self.previousEdge!.value + self.distance
       self.previousEdge!.nextEdge = self
@@ -29,6 +32,7 @@ class GridEdge {
       self.corner = 3
       self.distance = 0
       self.value = 1
+      self.radius = 0
     }
   }
 }
@@ -44,4 +48,20 @@ func fillEdges(_ maxNumber: Int) -> [GridEdge] {
 // first fill a list of edges
 let edges = fillEdges(inputNumber)
 
+for edge in edges {
+  print("Edge \(edge.value) at corner \(edge.corner) has radius \(edge.radius)")
+}
+
 // the correct starting point lies in between the last two edges
+let outerWidthHalf = (edges.last!.value - edges[edges.count - 2].value) / 2
+
+var distance = outerWidthHalf
+
+while distance + edges[edges.count - 2].value < inputNumber {
+  distance -= 1
+  print(distance)
+}
+
+distance += edges.last!.radius
+
+print(distance)
